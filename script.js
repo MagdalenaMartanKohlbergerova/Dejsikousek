fdb0602030f4c4082652e3fbdf441fac0f2f128e
Magdaléna Martan Kohlbergerová <magdalenaa.martankohlbergerova@gmail.com>
2024-05-30 17:38:31 +0200

    complete dejsikousek

---
+++ script.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+modal.addEventListener('innerHTML', () =>{
+    modal.innerHTML.close();
+})
+
+openModal.addEventListener('click', () =>{
+    modal.showModal();
+})
+
+closeModal.addEventListener('click', () =>{
+    modal.close();
+})
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active')
+}
